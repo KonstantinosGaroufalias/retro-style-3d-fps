ca93cdcb5ca8fe4e507b41b30fa2e24f3d169f6b
KonstantinosGaroufalias <101582855+KonstantinosGaroufalias@users.noreply.github.com>
2025-07-16 21:39:45 +0300

    Initial Commit

---
+++ retro-style-3d-fps-/characters/player/player.gd
@@ -0,0 +1,42 @@
+extends CharacterBody3D
+
+@onready var camera_3d = $Camera3D 
+@onready var character_mover = $CharacterMover
+@onready var health_manager = $HealthManager
+
+@export var mouse_sensitivity_h = 0.15
+@export var mouse_sensitivity_v = 0.15
+
+var dead = false
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	health_manager.died.connect(kill)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotation_degrees.y -= event.relative.x * mouse_sensitivity_h
+		camera_3d.rotation_degrees.x -= event.relative.y * mouse_sensitivity_v
+		camera_3d.rotation_degrees.x = clamp(camera_3d.rotation_degrees.x, -90, 90)
+
+func _process(delta):
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	if Input.is_action_just_pressed("restart"):
+		get_tree().reload_current_scene()
+	if Input.is_action_just_pressed("fullscreen"):
+		var fs = DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN
+		if fs:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forwards", "move_backwards")
+	var move_dir = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	character_mover.set_move_dir(move_dir)
+	if Input.is_action_just_pressed("jump"):
+		character_mover.jump()
+func kill():
+	dead = true
+	character_mover.set_move_dir(Vector3.ZERO)
