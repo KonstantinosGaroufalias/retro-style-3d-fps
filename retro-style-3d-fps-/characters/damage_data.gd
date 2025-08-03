b60e53ab081ff123a13e05f40d97f28a56bf7041
KonstantinosGaroufalias <101582855+KonstantinosGaroufalias@users.noreply.github.com>
2025-08-03 12:37:52 +0300

    Delete retro-style-3d-fps- directory

--- retro-style-3d-fps-/characters/damage_data.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-class_name DamageData
-
-var amount = 1
-var hit_pos : Vector3
--- retro-style-3d-fps-/weapons/bullet_emitters/burst_emitter.gd
+++
@@ -1,7 +0,0 @@
-extends BulletEmitter
-
-@export var burst_count = 5
-
-func fire():
-	for _i in range(burst_count):
-		super()
